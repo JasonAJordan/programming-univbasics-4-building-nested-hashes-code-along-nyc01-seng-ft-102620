7be75b1ac91d90dbca83cfe4aee008c2e3d2c8b6
Jason Jordan <jasonajordan0@gmail.com>
2020-10-02 00:11:56 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -10,9 +10,8 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {},
+      :heroine_friends => [],
       }
-    
   }
 
   
